03be950f145665a233ec9603234c9c311915a9be
MicaTeot <micaelat@itcsoluciones.com>
2024-05-08 16:11:12 -0300

    Debounce

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,7 +7,9 @@ import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@
   styles: [
   ]
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer: Subject<string> = new Subject<string>()
 
   @Input()
   public placeholder: string = '';
@@ -14,19 +17,37 @@ export class SearchBoxComponent {
   @Output()
   public onValue: EventEmitter<string> = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce: EventEmitter<string> = new EventEmitter<string>();
+
   @ViewChild('txtSearchInput') searchTerm!: ElementRef<HTMLInputElement>
+
+  ngOnInit(): void {
+    this.debouncer
+      .pipe(
+        debounceTime(1000)
+      )
+      .subscribe(value=>{
+        console.log('debouncer value', value);
+        this.onDebounce.emit(value);
+      })
+  }
 
   emittSearchValue(value: string): void {
     this.onValue.emit(value);
     this.searchTerm.nativeElement.value = '';
   }
 
+  onKeyPress(value: string){
+    // console.log(value);
+    this.debouncer.next(value);
+  }
   // * Esta manera emite el valor tomandolo desde el ts, en vez de pasarlo directamente desde el html como se ha hecho en le método
   // * que está por encima. Ambas maneras están bien.
 
   // emittSearchValue(): void {
-  //   const term = this.searchTerm.nativeElement.value;
-  //   this.onValue.emit(term);
-  //   this.searchTerm.nativeElement.value = '';
-  // }
+    //   const term = this.searchTerm.nativeElement.value;
+    //   this.onValue.emit(term);
+    //   this.searchTerm.nativeElement.value = '';
+    // }
 }
